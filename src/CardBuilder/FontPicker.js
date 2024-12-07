640f104edb3b02584e1011055daa053c41a72bea
Didem Gülşen Damlapınar <35114789+ddamlapinar@users.noreply.github.com>
2024-12-07 21:02:43 +0300

    feat: add more font options

--- src/CardBuilder/FontPicker.js
+++ src/CardBuilder/FontPicker.js
@@ -3,8 +3,13 @@ function FontPicker({ onSelectFont }) {
     <select id="fontPicker" onChange={onSelectFont}>
       <option value="Roboto">Roboto</option>
       <option value="Inter">Inter</option>
-      <option value="Inter">Montserrat</option>
-      <option value="PT Sans">Montserrat</option>
+      <option value="Montserrat">Montserrat</option>
+      <option value="PT Sans">PT Sans</option>
+      <option value="Arial">Arial</option>
+      <option value="Times New Roman">Times New Roman</option>
+      <option value="Courier New">Courier New</option>
+      <option value="Georgia">Georgia</option>
+      <option value="Verdana">Verdana</option>
     </select>
   );
 }

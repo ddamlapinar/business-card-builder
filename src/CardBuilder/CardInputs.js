1439e3d19b9f50bbd31ade6bbdb384a47188ecda
Didem Gülşen Damlapınar <35114789+ddamlapinar@users.noreply.github.com>
2024-12-07 17:47:38 +0300

    refactor: map form fields

--- src/CardBuilder/CardInputs.js
+++ src/CardBuilder/CardInputs.js
@@ -1,37 +1,35 @@
 import { CardFieldsContainer, Label, Input } from "./StyledCardBuilder";
 
+const FORM_FIELDS = [
+  { id:'firstName', name: 'firstName', label: 'Name', type: 'text', handler: 'handleInputChange' },
+  { id:'surname', name: 'surname', label: 'Surname', type: 'text', handler: 'handleInputChange' },
+  { id:'phone', name: 'phone', label: 'Phone', type: 'tel', handler: 'handleInputChange' },
+  { id:'company', name: 'company', label: 'Company Name', type: 'text', handler: 'handleInputChange' },
+  { id:'imageUrl', name: 'imageUrl', label: 'Image URL', type: 'url', handler: 'handleInputChange' },
+  { 
+    id:'imageFile',
+    name: 'imageFile', 
+    label: 'Or Upload Image', 
+    type: 'file', 
+    handler: 'handleFileInputChange',
+    accept: 'image/*'
+  },
+]
+
 const CardInputs = ({ handleFileInputChange, handleInputChange }) => {
   return (
     <CardFieldsContainer>
-      <Label htmlFor="firstName">
-        Name:
-        <Input type="text" name="firstName" onChange={handleInputChange} />
-      </Label>
-      <Label htmlFor="surname">
-        Surname:
-        <Input name="surname" type="text" onChange={handleInputChange} />
-      </Label>
-      <Label htmlFor="phone">
-        Phone:
-        <Input name="phone" type="phone" onChange={handleInputChange} />
-      </Label>
-      <Label htmlFor="company">
-        Company Name:
-        <Input name="company" type="text" onChange={handleInputChange} />
-      </Label>
-      <Label htmlFor="imageUrl">
-        Image URL:
-        <Input name="imageUrl" type="url" onChange={handleInputChange} />
-      </Label>
-      <Label htmlFor="imageFile">
-        Or Upload Image:
-        <Input
-          name="imageFile"
-          type="file"
-          accept="image/*"
-          onChange={handleFileInputChange}
-        />
-      </Label>
+          {FORM_FIELDS.map(({id,name,label,type,handler,...rest}) => (
+            <Label key={id} htmlFor={id}>
+              {label}
+              <Input id={id} 
+                type={type} 
+                name={name} 
+                onChange={handler === 'handleFileInputChange' ? handleFileInputChange : handleInputChange} 
+                {...rest}
+              />
+            </Label>
+          ))}
     </CardFieldsContainer>
   );
 };
